f636a2d180d2094f881f5912e78eb5a83007284d
rachmadaniHaryono <foreturiga@gmail.com>
2017-07-22 09:27:27 +0800

    chg: dev: update server
    - use get_print_result method to get tags
    - remove saved file to trash after processing

--- i2vec_cli/server.py
+++ i2vec_cli/server.py
@@ -1,10 +1,14 @@
+import os
 from flask import (
     Flask,
     render_template,
     request,
 )
+from send2trash import send2trash
 
 from i2vec_cli.requests_session import Session
+from i2vec_cli.__main__ import get_print_result
+from i2vec_cli.utils import user_data_dir
 
 
 app = Flask(__name__)
@@ -37,11 +41,14 @@ def convert_raw_to_flask_template(raw_input):
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    db_path = os.path.join(user_data_dir, 'main.db')
     session = Session()
     path = request.files['file']
     path.save(path.filename)
-    tags = session.get_tags(path=path.filename)
+    tags = get_print_result(
+        path=path.filename, db_path=db_path, format='dict', session=session)
     entries = convert_raw_to_flask_template(tags)
+    send2trash(path.filename)
     return render_template('upload_result.html', entries=entries)
 
 
